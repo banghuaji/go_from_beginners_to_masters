69652f5c450b39c26ff4ff9df31db6b23a61fd00
banghuaji <banghuaji@163.com>
2022-01-29 16:31:18 +0800

    chrome modify

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/tealeg/xlsx"
-	main2 "go_from_beginners_to_masters/lession1/init_go_project"
+	main2 "go_from_beginners_to_masters7777/lession1/init_go_project"
 )
 
 func main() {
